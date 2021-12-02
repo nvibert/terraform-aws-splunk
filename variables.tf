af5a57a80f4dbb1bc5352f274f00a1cb43c6a1aa
Nico Vibert <nico.vibert@hashicorp.com>
2021-12-02 18:13:40 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "region" { default = "eu-west-2" }
+variable "availability_zone" { default = "eu-west-2a" }
+variable "instance_type" { default = "t3.micro" }
+variable "private_ip" { default = "172.16.10.100" }
+variable "subnet" { default = "172.16.10.0/24" }
+variable "vpc_cidr" { default = "172.16.0.0/16" }
